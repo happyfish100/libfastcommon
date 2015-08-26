31149bb6e90ef58b1e3c6ffe6c2090543e35cfa9
yuqing <yuqing@yongche.com>
2015-08-26 18:45:10 +0800

    correct PTHREAD_MUTEX_ERRORCHECK define

--- src/common_define.h
+++ src/common_define.h
@@ -48,11 +48,11 @@ typedef void * (*ThreadEntranceFunc)(LPVOID lpThreadParameter);
 extern int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int kind);
 #endif
 
+#include "_os_define.h"
+
 #ifdef OS_LINUX
 #define PTHREAD_MUTEX_ERRORCHECK PTHREAD_MUTEX_ERRORCHECK_NP
 #endif
-
-#include "_os_define.h"
 
 #ifdef OS_BITS
   #if OS_BITS == 64
