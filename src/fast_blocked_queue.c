cb820096568a3a592dc30705ec35179629c547bb
yuqing <yuqing@yongche.com>
2016-01-26 09:42:53 +0800

    add fast_blocked_queue.[hc]

---
+++ src/fast_blocked_queue.c
@@ -0,0 +1,123 @@
+//fast_blocked_queue.c
+
+#include <errno.h>
+#include <pthread.h>
+#include <inttypes.h>
+#include "logger.h"
+#include "shared_func.h"
+#include "pthread_func.h"
+#include "fast_blocked_queue.h"
+
+int blocked_queue_init(struct fast_blocked_queue *pQueue)
+{
+	int result;
+
+	if ((result=init_pthread_lock(&(pQueue->lock))) != 0)
+	{
+		logError("file: "__FILE__", line: %d, "
+			"init_pthread_lock fail, errno: %d, error info: %s",
+			__LINE__, result, STRERROR(result));
+		return result;
+	}
+
+    result = pthread_cond_init(&(pQueue->cond), NULL);
+    if (result != 0)
+    {
+        logError("file: "__FILE__", line: %d, "
+                "pthread_cond_init fail, "
+                "errno: %d, error info: %s",
+                __LINE__, result, STRERROR(result));
+        return result;
+    }
+
+	pQueue->head = NULL;
+	pQueue->tail = NULL;
+
+	return 0;
+}
+
+int blocked_queue_push(struct fast_blocked_queue *pQueue,
+		struct fast_task_info *pTask)
+{
+	int result;
+    bool notify;
+
+	if ((result=pthread_mutex_lock(&(pQueue->lock))) != 0)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"call pthread_mutex_lock fail, " \
+			"errno: %d, error info: %s", \
+			__LINE__, result, STRERROR(result));
+		return result;
+	}
+
+	pTask->next = NULL;
+	if (pQueue->tail == NULL)
+	{
+		pQueue->head = pTask;
+        notify = true;
+	}
+	else
+	{
+		pQueue->tail->next = pTask;
+        notify = false;
+	}
+	pQueue->tail = pTask;
+
+	if ((result=pthread_mutex_unlock(&(pQueue->lock))) != 0)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"call pthread_mutex_unlock fail, " \
+			"errno: %d, error info: %s", \
+			__LINE__, result, STRERROR(result));
+	}
+
+    if (notify)
+    {
+        pthread_cond_signal(&(pQueue->cond));
+    }
+
+	return 0;
+}
+
+struct fast_task_info *blocked_queue_pop(struct fast_blocked_queue *pQueue)
+{
+	struct fast_task_info *pTask;
+	int result;
+
+	if ((result=pthread_mutex_lock(&(pQueue->lock))) != 0)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"call pthread_mutex_lock fail, " \
+			"errno: %d, error info: %s", \
+			__LINE__, result, STRERROR(result));
+		return NULL;
+	}
+
+	pTask = pQueue->head;
+	if (pTask == NULL)
+	{
+        pthread_cond_wait(&(pQueue->cond), &(pQueue->lock));
+        pTask = pQueue->head;
+    }
+
+	if (pTask != NULL)
+    {
+		pQueue->head = pTask->next;
+		if (pQueue->head == NULL)
+		{
+			pQueue->tail = NULL;
+		}
+	}
+
+	if ((result=pthread_mutex_unlock(&(pQueue->lock))) != 0)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"call pthread_mutex_unlock fail, " \
+			"errno: %d, error info: %s", \
+			__LINE__, result, STRERROR(result));
+	}
+
+	return pTask;
+}
+
---
+++ src/fast_blocked_queue.h
@@ -0,0 +1,43 @@
+/**
+* Copyright (C) 2008 Happy Fish / YuQing
+*
+* FastDFS may be copied only under the terms of the GNU General
+* Public License V3, which may be found in the FastDFS source kit.
+* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+**/
+
+//fast_blocked_queue.h
+
+#ifndef _FAST_BLOCKED_QUEUE_H
+#define _FAST_BLOCKED_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include "common_define.h"
+#include "fast_task_queue.h"
+
+struct fast_blocked_queue
+{
+	struct fast_task_info *head;
+	struct fast_task_info *tail;
+	pthread_mutex_t lock;
+    pthread_cond_t cond;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int blocked_queue_init(struct fast_blocked_queue *pQueue);
+int blocked_queue_push(struct fast_blocked_queue *pQueue,
+		struct fast_task_info *pTask);
+struct fast_task_info *blocked_queue_pop(struct fast_blocked_queue *pQueue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
