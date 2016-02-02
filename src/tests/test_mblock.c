b1a6bbfa276ab9347468ad64bbcddbbe219ec015
yuqing <yuqing@yongche.com>
2016-02-02 15:05:30 +0800

    output file device

--- src/tests/test_mblock.c
+++ src/tests/test_mblock.c
@@ -121,8 +121,9 @@ int main(int argc, char *argv[])
 		printf("procs: %d\n", info.procs);
 	}
 
-    stat("/dev/null", &st);
+    stat("/etc/passwd", &st);
     printf("file inode: %ld\n", (long)st.st_ino);
+    printf("file device: %"PRId64"\n", *((int64_t *)&st.st_dev));
     printf("file access time: %d.%ld\n", (int)st.st_atime, st.st_atimensec);
     printf("file modify time: %d.%ld\n", (int)st.st_mtime, st.st_mtimensec);
     printf("file change time: %d.%ld\n", (int)st.st_ctime, st.st_ctimensec);
