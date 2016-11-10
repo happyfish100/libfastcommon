decd1498d06b5bb28d754389849880e89bb13229
yuqing <yuqing@yongche.com>
2016-11-10 14:04:24 +0800

    add tests/test_ini_parser.c

---
+++ src/tests/test_ini_parser.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include "logger.h"
+#include "ini_file_reader.h"
+
+int main(int argc, char *argv[])
+{
+	int result;
+    IniContext context;
+    const char *szFilename = "/home/yuqing/watchd-config/order.conf";
+	
+	log_init();
+	g_log_context.log_level = LOG_DEBUG;
+
+    if ((result=iniLoadFromFile(szFilename, &context)) != 0)
+    {
+        return result;
+    }
+
+    iniPrintItems(&context);
+
+    iniFreeContext(&context);
+	return 0;
+}
+
