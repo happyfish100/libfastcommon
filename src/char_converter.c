e2735189a554480504b0be98b19749e47aaae70c
yuqing <yuqing@yongche.com>
2016-11-25 09:02:03 +0800

    add files: char_converter.h and char_converter.c

---
+++ src/char_converter.c
@@ -0,0 +1,87 @@
+/**
+* Copyright (C) 2008 Happy Fish / YuQing
+*
+* FastDFS may be copied only under the terms of the GNU General
+* Public License V3, which may be found in the FastDFS source kit.
+* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include "logger.h"
+#include "shared_func.h"
+#include "char_converter.h"
+
+int char_converter_init(FastCharConverter *pCharConverter,
+        const FastCharPair *charPairs, const int count)
+{
+    int i;
+    unsigned char src;
+    if (count > FAST_MAX_CHAR_COUNT)
+    {
+		logError("file: "__FILE__", line: %d, "
+                "count: %d is too large, exceeds %d!", __LINE__,
+                count, FAST_MAX_CHAR_COUNT);
+        return EINVAL;
+    }
+
+    pCharConverter->count = count;
+    for (i=0; i<count; i++)
+    {
+        src = charPairs[i].src;
+        pCharConverter->char_table[src] = charPairs[i].dest;
+    }
+    return 0;
+}
+
+int std_space_char_converter_init(FastCharConverter *pCharConverter,
+        const unsigned char dest_base)
+{
+#define SPACE_CHAR_PAIR_COUNT 7
+    int i;
+    FastCharPair pairs[SPACE_CHAR_PAIR_COUNT];
+
+    pairs[0].src = '\0';
+    pairs[1].src = '\t';
+    pairs[2].src = '\n';
+    pairs[3].src = '\v';
+    pairs[4].src = '\f';
+    pairs[5].src = '\r';
+    pairs[6].src = ' ';
+
+    for (i=0; i<SPACE_CHAR_PAIR_COUNT; i++) {
+        pairs[i].dest = dest_base + i;
+    }
+
+    return char_converter_init(pCharConverter, pairs, SPACE_CHAR_PAIR_COUNT);
+}
+
+int fast_char_convert(FastCharConverter *pCharConverter,
+        char *text, const int text_len)
+{
+    int count;
+    unsigned char *p;
+    unsigned char *end;
+
+    if (pCharConverter->count <= 0)
+    {
+        return 0;
+    }
+
+    count = 0;
+    end = (unsigned char *)text + text_len;
+    for (p=(unsigned char *)text; p<end; p++)
+    {
+        if (pCharConverter->char_table[*p] != 0)
+        {
+            *p = pCharConverter->char_table[*p];
+            ++count;
+        }
+    }
+
+    return count;
+}
+
---
+++ src/char_converter.h
@@ -0,0 +1,80 @@
+/**
+* Copyright (C) 2008 Happy Fish / YuQing
+*
+* FastDFS may be copied only under the terms of the GNU General
+* Public License V3, which may be found in the FastDFS source kit.
+* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+**/
+
+//char_converter.h
+#ifndef CHAR_CONVERTER_H
+#define CHAR_CONVERTER_H
+
+#include <syslog.h>
+#include <sys/time.h>
+#include "common_define.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FAST_MAX_CHAR_COUNT    256
+
+typedef struct fast_char_pair
+{
+    unsigned char src;
+    unsigned char dest;
+} FastCharPair;
+
+typedef struct fast_char_converter
+{
+    /*
+     * char pairs count
+     * */
+    int  count;
+
+    /*
+     * char table to convert
+     * */
+    unsigned char char_table[FAST_MAX_CHAR_COUNT];
+} FastCharConverter;
+
+/**
+ *  char converter init function
+ *  parameters:
+ *           pCharConverter: the char converter
+ *           charPairs: the char pairs
+ *           count: the count of char pairs
+ *  return: 0 for success, != 0 fail
+*/
+int char_converter_init(FastCharConverter *pCharConverter,
+        const FastCharPair *charPairs, const int count);
+
+/**
+ *  standard space chars to convert
+ *  parameters:
+ *           pCharConverter: the char converter
+ *           dest_base: the dest base char
+ *  return: 0 for success, != 0 fail
+*/
+int std_space_char_converter_init(FastCharConverter *pCharConverter,
+        const unsigned char dest_base);
+
+/**
+ *  char convert function
+ *  parameters:
+ *           pCharConverter: the char converter
+ *           text: the text to convert
+ *           text_len: the length of text
+ *  return: converted char count
+*/
+int fast_char_convert(FastCharConverter *pCharConverter,
+        char *text, const int text_len);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
