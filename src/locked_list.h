6a3bcd45477ab20726815085dee93f8cd445dc95
YuQing <384681@qq.com>
2021-04-09 21:28:22 +0800

    add file locked_list.h

---
+++ src/locked_list.h
@@ -0,0 +1,55 @@
+#ifndef _LOCKED_LIST_H
+#define _LOCKED_LIST_H
+
+#include "fc_list.h"
+#include "pthread_func.h"
+
+typedef struct fc_locked_list {
+    struct fc_list_head head;
+    pthread_mutex_t lock;
+} FCLockedList;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    static inline int locked_list_init(FCLockedList *list)
+    {
+        int result;
+        if ((result=init_pthread_lock(&list->lock)) != 0) {
+            return result;
+        }
+
+        FC_INIT_LIST_HEAD(&list->head);
+        return 0;
+    }
+
+    static inline void locked_list_add(struct fc_list_head *_new,
+            FCLockedList *list)
+    {
+        PTHREAD_MUTEX_LOCK(&list->lock);
+        fc_list_add(_new, &list->head);
+        PTHREAD_MUTEX_UNLOCK(&list->lock);
+    }
+
+    static inline void locked_list_add_tail(struct fc_list_head *_new,
+            FCLockedList *list)
+    {
+        PTHREAD_MUTEX_LOCK(&list->lock);
+        fc_list_add_tail(_new, &list->head);
+        PTHREAD_MUTEX_UNLOCK(&list->lock);
+    }
+
+    static inline void locked_list_del(struct fc_list_head *old,
+            FCLockedList *list)
+    {
+        PTHREAD_MUTEX_LOCK(&list->lock);
+        fc_list_del_init(old);
+        PTHREAD_MUTEX_UNLOCK(&list->lock);
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
